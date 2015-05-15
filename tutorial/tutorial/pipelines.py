333cd893bb5075136286f9f7747075fa0441f73a
jzorrof@gmail.com <jzorrof@gmail.com>
2015-05-15 14:47:24 +0800

    update

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -13,10 +13,15 @@ from scrapy.xlib.pydispatch import dispatcher
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
+
 class TutorialPipeline(object):
     def __init__(self):
-    	dispatcher.connect(self.spider_closed, signals.spider_closed)
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
         self.file = codecs.open('qiche.json', 'wb', encoding='utf-8')
+        self.bucketname = 'scrapy_data_2'
+        self.conn = S3Connection()
+        self.getbucket = self.conn.get_bucket(self.bucketname)
+        self.k = Key(self.getbucket)
 
     def process_item(self, item, spider):
         if item['desc']:
@@ -25,19 +30,14 @@ class TutorialPipeline(object):
             return item
         else:
             pass
-            #raise DropItem("Missing price in %s" % item)
+            # raise DropItem("Missing price in %s" % item)
 
     def spider_closed(self, spider):
-    	print('closing~~~~~~~~')
-    	'''
+        print('closing~~~~~~~~')
+        '''
         connection to Amazon S3
         android create bucket for story scrapy_data
         '''
-        self.bucketname = 'scrapy_data_2'
-        self.conn = S3Connection()
-        self.bucket = self.conn.create_bucket(self.bucketname)
-        self.getbucket =self.conn.get_bucket(self.bucketname)
-        self.k = Key(self.getbucket)
         self.k.key = 'my_scrapy'
         self.k.set_contents_from_filename('qiche.json')
         self.file.close()
